08b2e057742b7539a0d23289623e47aa77bbf5a8
vamsi <vamsikiran.mekala2@mail.dcu.ie>
2025-12-16 13:54:37 +0000

    Final version for GPIO-basic

--- main.cpp
+++ main.cpp
@@ -5,17 +5,17 @@
 int main() {
     //Initializing two GPIO Pins
     GPIO out(17);
-    GPIO in(27);
-
-    //setting their directions
     out.setDirection(GPIO::OUT);
+    GPIO in(27);
     in.setDirection(GPIO::IN);
 
     //test
+    //write high to GPIO17 then read GPIO 27
     out.write(true); //set output to high
     sleep(1);  //wait signal to stabilize
     std::cout << "Input reads (should be 1)" << in.read() << std::endl;
 
+    //write low to GPIO 17 the read to GPIO27
     out.write(false); //set output to low
     sleep(1);
     std::cout << "Input reads (should be 0)" << in.read() << std::endl;
